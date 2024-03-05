cdcec893d68ec7bd42183d6f2db70e49ac65a461
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-05 14:53:29 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace TrueOrFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do not edit these lines
+            Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+            string entry = Console.ReadLine();
+
+            // Type your code below
+            string[] questions = new string[] { "An eggplant is also known as an aubergine.", "Eggplants are a species in the nightshade family.", "According to botanical definition, eggplant is a vegetable.", "When cut open, eggplants do not brown (oxidation)", "Eggplants were originalyl domesticated from the wild species bitter apple" };
+
+            bool[] answers = new bool[]
+            {
+        true,
+        true,
+        false,
+        false,
+        true
+            };
+
+            bool[] responses = new bool[questions.Length];
+
+            if (questions.Length != answers.Length)
+            {
+                Console.WriteLine("Warning! The number of answers supplied does not match the number of questions.");
+            }
+
+            int askingIndex = 0;
+            foreach (string question in questions)
+            {
+                string input;
+                bool isBool;
+                bool inputBool;
+                Console.WriteLine(question);
+                Console.WriteLine("True or false?");
+                input = Console.ReadLine();
+                isBool = Boolean.TryParse(input, out inputBool);
+                while (!isBool)
+                {
+                    Console.WriteLine("Please respond with 'true' or 'false'");
+                    input = Console.ReadLine();
+                    isBool = Boolean.TryParse(input, out inputBool);
+                }
+                responses[askingIndex] = inputBool;
+                askingIndex++;
+            }
+
+            int scoringIndex = 0;
+            int score = 0;
+
+            foreach (bool answer in answers)
+            {
+                bool response = responses[scoringIndex];
+                Console.Write(scoringIndex + 1 + ".");
+                Console.WriteLine($"Input: {response} | Answer: {answer}");
+
+                if (response == answer)
+                {
+                    score++;
+                }
+                scoringIndex++;
+
+            }
+
+            Console.WriteLine($"You got {score} out of {questions.Length} correct.");
+
+        }
+    }
+}
